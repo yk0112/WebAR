bc648ba20923d57121a9f86a7a231f92b4450600
yk0112 <yosikou0726@gmail.com>
2023-07-28 01:29:17 +0900

    debug

--- myapp/settings.py
+++ myapp/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-
+SECRET_KEY='django-insecure-u%)@v!6qr7di-nwo*@7u6yny@z3^=*=_^etvx(5sx6w^4ij9r&'
+GOOGLEMAP_API_KEY='AIzaSyAmDjN0Pu_ty8jL2EY3qfX4HST-zKw_Gj0'
 
 
 
