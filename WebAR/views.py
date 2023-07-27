be025eb80132e4712d719e4d8f41fc1986781839
yk0112 <yosikou0726@gmail.com>
2023-07-28 01:55:50 +0900

    debug

--- WebAR/views.py
+++ WebAR/views.py
@@ -1,6 +1,6 @@
 import sys
 import json
-from myapp.local_settings import *
+from myapp.settings import *
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
--- myapp/settings.py
+++ myapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY='django-insecure-u%)@v!6qr7di-nwo*@7u6yny@z3^=*=_^etvx(5sx6w^4ij9r&'
 
-
+GOOGLEMAP_API_KEY='AIzaSyAmDjN0Pu_ty8jL2EY3qfX4HST-zKw_Gj0'
 # Application definition
 
 INSTALLED_APPS = [
