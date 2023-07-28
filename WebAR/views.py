5b3f53e38f465161f5c106e2225ddb931d529033
yk0112 <yosikou0726@gmail.com>
2023-07-28 20:20:03 +0900

    debug

--- WebAR/views.py
+++ WebAR/views.py
@@ -1,6 +1,6 @@
 import sys
 import json
-from myapp.settings import *
+from app.settings import *
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
